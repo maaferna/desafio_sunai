173abe3f586fe5339209b4965e880daf3bdfb970
mafparra <mafernandezparra@gmail.com>
2022-11-17 11:48:31 -0300

    Initial commit -m

---
+++ README.md
@@ -0,0 +1,2 @@
+# solar_production
+En esta oportunidad, contarás con 2 archivos (.xlsx), donde cada uno representa la generación de energía en una planta solar de 1 día completo, dividida por inversor. La relación entre inversor y planta es de muchos a uno, es decir, una planta está relacionada a 1 o más inversores.
---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == "__main__":
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
